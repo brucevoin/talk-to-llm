0116ec99dc6faf432d174a495d22401cb796e886
fenghaichun <brucevoin@icloud.com>
2024-03-27 09:44:14 +0800

    Refactor

--- talk_to_llm/audio_output.py
+++ talk_to_llm/audio_output.py
@@ -3,27 +3,30 @@ Description:
 Author: haichun feng
 Date: 2024-03-26 16:06:49
 LastEditor: haichun feng
-LastEditTime: 2024-03-26 18:13:11
+LastEditTime: 2024-03-27 09:39:23
 '''
 
 from playsound import playsound
 from TTS.api import TTS
+
+from config_reader import Get_AUDIO_FILES_DIRECTORY
 
 
 class AudioOutput:
     def __init__(self):
         print("loading TTS model")
         self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+        self.AUDIO_FILES_DIRECTORY = Get_AUDIO_FILES_DIRECTORY()
 
     def speak(self, text_str):
 
-        output = "/Users/fhc/Downloads/output.wav"
+        output = f"{self.AUDIO_FILES_DIRECTORY}/audio_output.wav"
         print("saving tts to file")
         # speaker_wav用以指定克隆对象的声音样本
         self.tts.tts_to_file(
             text=text_str,
             file_path=output,
-            speaker_wav=["/Users/fhc/Downloads/Untitled.wav"],
+            speaker_wav=["test_data/voice_clone_sample.wav"],
             language="zh-cn",
             split_sentences=True,
             emotion="angry",
