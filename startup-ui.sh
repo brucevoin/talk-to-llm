578497e3d7501deac0f9fa2379b5d2961033a53f
fenghaichun <brucevoin@icloud.com>
2024-03-29 16:07:03 +0800

    add startup ui script

---
+++ startup-ui.sh
@@ -0,0 +1,8 @@
+###
+ # @Description: 
+ # @Author: haichun feng
+ # @Date: 2024-03-29 16:05:48
+ # @LastEditor: haichun feng
+ # @LastEditTime: 2024-03-29 16:05:54
+### 
+streamlit run talk_to_llm/ui.py
